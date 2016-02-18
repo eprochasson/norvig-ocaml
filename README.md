fb6d411c50696dbbd22482016a4465d2e47c0688
Emmanuel Prochasson <eprochasson@gmail.com>
2016-02-18 17:23:04 +0800

    it works \o/

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # OCaml Sudoku Solver
 
 Based on the algorithm described (http://norvig.com/sudoku.html)[here].
+
+It works. And is indeed blazing fast.
+
+Licence: MIT
--- src/sudoku.ml
+++ src/sudoku.ml
@@ -5,6 +5,9 @@ open Batteries
 
 exception Invalid_grid of string
 
+(***********************************************************************************************************************
+ Useful definitions
+***********************************************************************************************************************)
 (* Cartesian product *)
 let cross (a: 'a list) (b : 'b list): ('a * 'b) list = List.flatten (List.map (fun ia -> List.map (fun ib -> (ia, ib)) b) a)
 
@@ -37,6 +40,19 @@ let peers (coord: char * int): (char * int) list =
 
 (* Values of a square *)
 type square_value = int list
+
+(***********************************************************************************************************************
+ Helper functions
+***********************************************************************************************************************)
+let is_solved arr: bool =
+  let array_all = (fun fn arr -> Array.fold_left (fun acc e -> acc && (fn e)) true arr) in
+  array_all (fun e -> (List.length e) = 1) arr (* Each square has only one value: problem solved *)
+
+let rec list_first_that_returns (fn: 'a -> 'b option) (l: 'a list): 'b option = match l with
+  | [] -> None
+  | hd::tl -> let t = (fn hd) in match t with
+    | None -> list_first_that_returns fn tl
+    | Some x -> Some x
 
 let sq_value_to_string (sq: int list): string = List.fold_left (fun acc s -> acc ^ (string_of_int s)) "" sq
 
@@ -76,11 +92,39 @@ let grid_values (grid:string): int option array =
   ) (String.to_list filtered) in
   Array.of_list g
 
+(* Add space at the end of a string to make it of size i *)
+let rec pad str i c = if (String.length str) = i then str else pad (str ^ c) i c
+let display_grid arr: unit =
+  let width_column = Array.fold_left (fun acc e1 -> if acc >= List.length e1 then acc else List.length e1) 0 arr in
+  let arr' = Array.map (fun s -> sq_value_to_string s) arr in
+  let () = Array.iteri (fun i _ ->
+    if (i mod (9*3)) = 0 then print_string ("\n" ^ (pad "-" ((width_column * 9) + 2) "-") ^ "\n")
+    else if (i mod 9) = 0 then print_string "\n"
+    else if (i mod 3) = 0 then print_string "|" else ();
+    print_string (pad arr'.(i) width_column " ")
+  ) arr' in
+  print_string "\n"
+
+(* Square containing more than one value are undecided: retrieve the undecided square with the least number of values *)
+let get_smallest_indecision_square arr: int option =
+  let arr' = Array.mapi (fun i e -> (i, List.length e)) arr in
+  let (is, _) = Array.fold_left (fun acc e ->
+    let sq, len = acc in (* int option * int *)
+    let sq',len' = e in (* int * int *)
+    match sq with
+    | None -> if (len' > 1) then (Some sq', len') else acc
+    | Some x -> if (len' > 1) && (len' < len) then (Some sq', len') else acc
+  ) (None, 0) arr' in
+  is
+
+(***********************************************************************************************************************
+  Problem solving starts here
+***********************************************************************************************************************)
 let rec eliminate (i:int) (d: int) (arr: square_value array): unit =
   if not (is_in d arr.(i))
   then () (* Nothing to eliminate *)
   else
-    let () = arr.(i) <- List.filter (fun el -> el <> d) arr.(i) in
+    let () = arr.(i) <- List.filter (fun el -> el <> d) arr.(i) in (* Leave all values but the one to eliminate *)
     let vals = arr.(i) in match vals with
     (* No more possible values something went wrong *)
     | [] -> raise (Invalid_grid "Eliminated last possible value for a square: grid can not be solved")
@@ -116,24 +160,45 @@ let parse_grid (grid:string): int list array =
   ) grid in
   res
 
-(* Add space at the end of a string to make it of size i *)
-let rec pad str i c = if (String.length str) = i then str else pad (str ^ c) i c
+let rec search arr: int list array =
+  if is_solved arr then arr
+  else
+    (* Get the square with the least number of indecided values, pick the first one and see what happens *)
+    let is = get_smallest_indecision_square arr in
+    let is = match is with
+      | None -> raise (Invalid_grid "It shouldn't happen") (* if it happens, it means there are no undecided square
+                                                              left, meaning the problem is solved, meaning we exited
+                                                              earlier *)
+      | Some x -> x
+    in
+    let choices = arr.(is) in (* The value we could assign to this square, we'll test them all *)
+    let response = list_first_that_returns (fun v -> (* Stops when an valid answer has been found *)
+      let arr_copy = Array.copy arr in (* Copy the array *)
+      try
+        let () = assign is v arr_copy in (* Try the assign/eliminate dance (raises an exception if a value is invalid) *)
+        let res = search arr_copy in (* Move to the next value to pivot *)
+        Some res
+      with
+        Invalid_grid _ -> None
+    ) choices in
+    match response with
+    | None -> raise (Invalid_grid "This grid admits no solution")
+    | Some x -> x
 
-let display_grid arr: unit =
-  let width_column = Array.fold_left (fun acc e1 -> if acc >= List.length e1 then acc else List.length e1) 0 arr in
-  let arr' = Array.map (fun s -> sq_value_to_string s) arr in
-  let () = Array.iteri (fun i _ ->
-    if (i mod (9*3)) = 0 then print_string ("\n" ^ (pad "-" ((width_column * 9) + 2) "-") ^ "\n")
-    else if (i mod 9) = 0 then print_string "\n"
-    else if (i mod 3) = 0 then print_string "|" else ();
-    print_string (pad arr'.(i) width_column " ")
-  ) arr' in
-  print_string "\n"
-
-
-
+let solve grid = search (parse_grid grid)
 
 let () =
-  (* let g = "003020600900305001001806400008102900700000008006708200002609500800203009005010300" in *)
-  let g' = "4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......" in
-  display_grid (parse_grid g')
+  let hardest = [
+  "85...24..72......9..4.........1.7..23.5...9...4...........8..7..17..........36.4." ;
+  "..53.....8......2..7..1.5..4....53...1..7...6..32...8..6.5....9..4....3......97.." ;
+  "12..4......5.69.1...9...5.........7.7...52.9..3......2.9.6...5.4..9..8.1..3...9.4" ;
+  "...57..3.1......2.7...234......8...4..7..4...49....6.5.42...3.....7..9....18....." ;
+  "7..1523........92....3.....1....47.8.......6............9...5.6.4.9.7...8....6.1." ;
+  "1....7.9..3..2...8..96..5....53..9...1..8...26....4...3......1..4......7..7...3.." ;
+  "1...34.8....8..5....4.6..21.18......3..1.2..6......81.52..7.9....6..9....9.64...2" ;
+  "...92......68.3...19..7...623..4.1....1...7....8.3..297...8..91...5.72......64..." ;
+  ".6.5.4.3.1...9...8.........9...5...6.4.6.2.7.7...4...5.........4...8...1.5.2.3.4." ;
+  "7.....4...2..7..8...3..8.799..5..3...6..2..9...1.97..6...3..9...3..4..6...9..1.35" ;
+  "....7..2.8.......6.1.2.5...9.54....8.........3....85.1...3.2.8.4.......9.7..6...." ;
+  ] in
+  List.iter (fun p -> display_grid (solve p)) hardest
